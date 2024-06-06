79fe77e8a52cabef919739a1c7f8e322b7858312
Lumila <lumilatourn@gmail.com>
2024-06-06 20:09:53 -0300

    agregado activities

--- app/src/main/java/com/example/clubdeportivopractica/IdCard.kt
+++ app/src/main/java/com/example/clubdeportivopractica/IdCard.kt
@@ -1,29 +1,20 @@
 package com.example.clubdeportivopractica
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class PaymentActivity : AppCompatActivity() {
+class IdCard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_payment)
+        setContentView(R.layout.activity_id_card)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btnPayment: Button =findViewById(R.id.btnPay)
-
-        btnPayment.setOnClickListener {
-            val intent = Intent(this@PaymentActivity, PaymentReceipt::class.java)
-            startActivity(intent)
         }
     }
 }
