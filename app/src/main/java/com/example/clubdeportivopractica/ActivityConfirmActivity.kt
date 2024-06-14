31841eefdd6dd4e13baf55d7a2d3bd5a5ee8bd3c
vivi_ <vivianavercesi22@gmail.com>
2024-06-14 07:57:27 -0300

    Button added

--- app/src/main/java/com/example/clubdeportivopractica/ActivityConfirmActivity.kt
+++ app/src/main/java/com/example/clubdeportivopractica/ActivityConfirmActivity.kt
@@ -8,25 +8,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class SignUpActivitiesActivity : AppCompatActivity() {
+class ActivityConfirmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up_activities)
+        setContentView(R.layout.activity_confirm)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val button1 = findViewById<Button>(R.id.btnActivitySignUp)
-        button1.setOnClickListener{
-            val intent1 = Intent(this, ActivityConfirmActivity::class.java)
-            startActivity(intent1)
-        }
-        val button2 = findViewById<Button>(R.id.btnBackAdmM)
-        button2.setOnClickListener{
-            val intent2 = Intent(this, AdminMenuActivity::class.java)
-            startActivity(intent2)
+        val button = findViewById<Button>(R.id.btnBackAdminMe)
+        button.setOnClickListener{
+            val intent = Intent(this, AdminMenuActivity::class.java)
+            startActivity(intent)
         }
     }
 }
