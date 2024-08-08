c8967a61846a2ae4f4d670fa440064f9544d6b22
vivi_ <vivianavercesi22@gmail.com>
2024-08-08 16:04:39 -0300

    User Login

--- app/src/main/java/com/example/clubdeportivopractica/LogInActivity.kt
+++ app/src/main/java/com/example/clubdeportivopractica/LogInActivity.kt
@@ -3,6 +3,8 @@ package com.example.clubdeportivopractica
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,13 +12,29 @@ import androidx.core.view.WindowInsetsCompat
 
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+       lateinit var databaseHelper: DatabaseHelper
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+        val usernameEditText = findViewById<EditText>(R.id.txtUser)
+        val passwordEditText = findViewById<EditText>(R.id.txtPass)
+        val loginButton = findViewById<Button>(R.id.btnLogIn)
+
+        databaseHelper = DatabaseHelper(this)
+
         val button = findViewById<Button>(R.id.btnLogIn)
-        button.setOnClickListener{
-            val intent = Intent(this, AdminMenuActivity::class.java)
-            startActivity(intent)
+        button.setOnClickListener {
+            val username = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+            if (databaseHelper.validateUser(username, password)) {
+                val intent = Intent(this, AdminMenuActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Usuario o contraseña incorrectos", Toast.LENGTH_LONG).show()
+            }
         }
 
     }
---
+++ app/src/main/java/com/example/clubdeportivopractica/User.kt
@@ -0,0 +1,7 @@
+package com.example.clubdeportivopractica
+
+class User(val id: Int, val name: String) {
+    override fun toString(): String {
+        return "User($id, $name)"
+    }
+}
